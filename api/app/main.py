77157e18d9aa6e6e78d5df4163315a644dccf377
Your Name <you@example.com>
2024-09-19 19:22:53 +0200

    add Endpoints for hierachical get and set of model_archiecture

--- api/app/main.py
+++ api/app/main.py
@@ -1,5 +1,6 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from typing import List, Optional
 
 app = FastAPI()
 
@@ -9,9 +10,40 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/tut")
-def read_item():
+@app.get("/get/{levels:path}")
+async def read_item(levels: str, q: Optional[str] = None):
+    # Split the levels by "/" to get a list of them
+    level_list = levels.split("/")
+    # Combine all levels into a single string
+    item_id = " ".join(level_list)
+
+    if q:
+        return {"item": item_id, "q": q}
+    return {"item": item_id}
+
+@app.post("/set/{levels:path}")
+async def read_item(request: Request, levels: str, q: Optional[str] = None):
+    test = await request.json
+    # Split the levels by "/" to get a list of them
+    level_list = levels.split("/")
+    # Combine all levels into a single string
+    item_id = " ".join(level_list)
+    item_id += str(test)
+    if q:
+        return {"item": item_id, "q": q}
+    return {"item": item_id}
+@app.get("/get/Crawler/lance")
+def getCrawlerInfo():
     return "Tat"
+
+@app.get("/get/Crawler/")
+def getCrawlerInfo():
+    return "Tat"
+@app.post("/set/Crawler")
+def setCrawlerStats():
+    return "Tat"
+
+
 
 
 if __name__ == "__main__":
